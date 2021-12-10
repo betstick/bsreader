621f77800f11f8ec28791f31e3be7c38e1ff9202
betstick <61669265+betstick@users.noreply.github.com>
2021-12-10 17:24:59 -0500

    cleaned up test unit

--- test.cpp
+++ test.cpp
@@ -1,36 +1,24 @@
 #include "bsreader.hpp"
-#include <random>
-#include <time.h>
 
 int main()
 {
-	time_t time;
-	srand(time);
-	//printf("rand:%i\n",rand());
-
 	char buffer[4];
 	BSReader reader = BSReader("../testfile",buffer,4);
 	char num[2];
 
-	reader.debug();
 	reader.read(num,2);
-	//printf("buffer: %i\n",buffer[1]);
-	//reader.debug();
 	printf("chars: %c%c\n",num[0],num[1]);
 	
-	reader.debug();
 	reader.read(num,2);
 	printf("chars: %c%c\n",num[0],num[1]);
-	//reader.debug();
-	reader.debug();
-	reader.read(num,2);
-	printf("chars: %c%c\n",num[0],num[1]);
-	//reader.debug();
 
-	reader.debug();
+	reader.read(num,2);
+	printf("chars: %c%c\n",num[0],num[1]);
+
 	reader.read(num,2);
 	printf("chars: %c%c\n",num[0],num[1]);
 
 	reader.seek(2,0);
-	reader.debug();
+	reader.read(num,2);
+	printf("chars: %c%c\n",num[0],num[1]);
 };

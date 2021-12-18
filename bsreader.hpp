efb54254f4bea38bd29e7db879af63298d191c84
betstick <61669265+betstick@users.noreply.github.com>
2021-12-17 21:25:20 -0500

    full rewrite

--- bsreader.hpp
+++ bsreader.hpp
@@ -7,194 +7,106 @@
 #include <stdexcept>
 #include <bits/stdc++.h> //is this standard?
 
-/*
-This reader is designed to function similarly to the fread()
-function but with a backend buffer that you don't need to
-worry about. It'll handle buffer management to speed up 
-reads and prevent some disk thrashing.
-
-Recommended buffer size is around 4096 bytes. You can use more
-if you want, but I'm not sure if it'll help or hurt.
-
-Calling stepout() without having stepped in is undefined
-behaviour. Don't do it if you value your sanity.
-
-Unless otherwise specified all sizes are in bytes.
-
-Buffer is kept private since touching it is dangerous. It is
-supposed to be read from and only filled from the original
-file. So writing to it isn't needed. Also no real reason to
-read directly from it.
-
-Use fileSize member as a "SEEK_END" for the seek method.
-Use 0 as a "SEEK_SET" and position as "SEEK_CUR".
-*/
-
 class BSReader
 {
-    public: //public for use in seek
-    uint64_t position = 0; //SEEK_CUR current position of the "read head"
-    uint64_t fileSize; //SEEK_END later
-    //just use 0 for SEEK_SET
+	private:
+	FILE* file;
+	char* buffer;
+	uint64_t bufferSize;
+	uint64_t bufferPos;
 
-    private:
-    uint64_t offset = 0; //buffer start position relative to file in bytes
-    
-    FILE* file;
-
-    char* buffer; //ptr to arbitrarily sized buffer
-    uint64_t bufferSize; //size of said buffer in bytes
-
-	//queue to allow nested stepin/stepouts 
 	std::queue<uint64_t> stepStack[64]; 
 
     public:
+	uint64_t fileSize;
+	uint64_t readPos;
 
-	BSReader(){}; //needed a default that did nothing :/
+	BSReader(){};
 
-	//uses an internal buffer to be safe.
-	BSReader(std::string filePath, size_t buffSize)
+	BSReader(std::string filePath, uint64_t bufferSizeIn)
 	{
 		file = fopen(filePath.c_str(),"rb");
 
 		if(file == NULL)
-			throw std::runtime_error("no file");
+			throw std::runtime_error("Failed to open file!\n");
 
-		buffer = new char[buffSize]; //throw buffer onto heap
-
-		//init values
-		getSize();
-		position = 0;
-		offset = 0;
-		bufferSize = buffSize;
-
-		//initial buffer fill
-		fread(buffer,bufferSize,1,file);
+		bufferSize = bufferSizeIn;
+		buffer = new char[bufferSize];
 	};
 
-	//destructor, needed cause we mess with the heap!
 	~BSReader()
 	{
 		delete[] buffer;
 	};
 
-    void read(void* dest, size_t size)
-    {
-#ifndef BLEED //these checks are not strictly needed, good code won't need them.
-		//out of bounds position
-		if(position > fileSize)
-			throw std::runtime_error("position larger than file!\n");
+	void read(void* dest, uint64_t readSize)
+	{
+		//do first read, run second until not needed, if needed do third
+		bool readIsTooBig = (bufferPos + bufferSize) - readPos < readSize;
 
-		//size is negative, shouldn't be possible?
-		/*if(size < 0)
-			throw std::runtime_error("invalid target size!\n");*/
-#endif
-
-		//if size is greater than the buffer size
-		if(size > bufferSize)
+		uint64_t startSize = readIsTooBig ? (bufferPos+bufferSize) - readPos : readSize;
+		memcpy(dest,buffer+(readPos-bufferPos),startSize);
+		readPos += startSize;
+		bufferAutoAdjust();
+		
+		if(readIsTooBig)
 		{
-			//more janky int math, division rounds down
-			uint64_t chunks = size / bufferSize; //how many buffer sizes needed
-			uint64_t remainder = size - (chunks * bufferSize); //amount of data left over
+			uint64_t fullReads = (readSize - startSize) / bufferSize;
 
-			for(uint64_t i = 0; i < chunks; i++)
+			for(int i = 0; i < fullReads; i++)
 			{
-				read(&dest+(i*bufferSize),bufferSize);
+				memcpy(dest+readPos-bufferPos,buffer+(readPos-bufferPos),bufferSize);
+				readPos += bufferSize;
+				bufferAutoAdjust();
 			}
 
-			//read the remainder onto end if needed
-			if(remainder != 0)
-				read(&dest+(chunks*bufferSize),remainder);
-		}
-		else
-		{
-			uint64_t relativePos = offset > 0 ? position - offset : position;
-			int64_t bufferEnd = offset + bufferSize; //end byte of buffer
-			int64_t overflow = (position + size) - bufferEnd;
+			uint64_t remainder = fullReads * bufferSize + startSize - readSize;
 
-			//if target is within buffer
-			if(overflow < 0) //thefifthmatt figured this out. very clever
+			if(remainder > 0)
 			{
-				memcpy(dest,buffer+relativePos,size);
-				position += size;
-
-				if(position >= bufferEnd)
-					bufferSet();
-			}
-			else //target DOES NOT end within the buffer
-			{
-				int64_t early = size - overflow;
-				memcpy(dest,buffer+relativePos,early);
-				seek(bufferEnd,0);
-				memcpy(dest,buffer+early,overflow);
+				memcpy(dest+readPos-bufferPos,buffer+(readPos-bufferPos),remainder);
+				readPos += remainder;
+				bufferAutoAdjust();
 			}
 		}
-    };
-
-    void seek(uint64_t positionAdjust, int64_t origin)
-    {
-        //calculate what buffer is needed to be loaded
-        position = positionAdjust + origin;
-
-        //pos not within current buffer
-        if(position >= offset + bufferSize || position < offset)
-        {
-            //because ints, this eliminates remainders.
-            //there's probably a math function to do this.
-            offset = (position / bufferSize) * bufferSize;
-        }
-
-        bufferSet(); //fill buffer based on new offset
-    };
-
-	void stepIn(uint64_t targetPosition)
-	{
-		stepStack->push(position);
-		seek(targetPosition,0);
 	};
 
-	void stepOut()
+	private:
+	void setBufferPosition(uint64_t position)
 	{
-		seek(stepStack->front(),0);
-		stepStack->pop();
+		bufferPos = position;
+		fseek(file,bufferPos,SEEK_SET);
+		refillBuffer();
 	};
 
-    private:
-    
+	void refillBuffer()
+	{
+		fread(buffer,bufferSize,1,file);
+	};
+
+	void bufferAutoAdjust()
+	{
+		setBufferPosition((readPos / bufferSize) * bufferSize);
+	};
+
 	void getSize()
 	{
 		fseek(file,0,SEEK_END);
 		fileSize = ftell(file);
 		rewind(file);
-		
-		printf("Filesize: %i bytes\n",fileSize);
 	};
 
-    void bufferSet() //refills buffer based on offset
-    {
-#ifdef DEBUG
-		//printf("offset: 0x%x\t\t%i\n",offset,offset);
-#endif 
-		if(offset + bufferSize >= fileSize)
-		{
-			printf("offset: 0x%x\t\t%i\n",offset,offset);
-			throw std::runtime_error("EOF\n");
-		}
+	void stepIn(uint64_t targetPosition)
+	{
+		stepStack->push(readPos);
+		readPos = targetPosition;
+		bufferAutoAdjust();
+	};
 
-        fseek(file,offset,SEEK_SET);
-
-        if(fread(buffer,bufferSize,1,file)==0)
-		{
-			//if the buffer is GREATER this is normal behavior
-			if(feof(file) && bufferSize <= fileSize)
-				throw std::runtime_error("EOF");
-			
-			if(ferror(file))
-			{
-				printf("bufsz:%i\tofst:%i\n",bufferSize,offset);
-				printf("i'm stupid\n");
-				throw std::runtime_error("ferror");
-			}
-		}
-    };
+	void stepOut()
+	{
+		readPos = stepStack->front();
+		stepStack->pop();
+		bufferAutoAdjust();
+	};
 };
